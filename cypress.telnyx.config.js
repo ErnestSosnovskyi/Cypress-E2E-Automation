38e110d21be4a889c1ce93cea6a21a720b0f03d1
ErnestSosnovskyi <e.sosnovskyi@student.bachelor-goit.com.ua>
2026-06-17 00:42:22 +0200

    fixing support commit

--- cypress.telnyx.config.js
+++ cypress.telnyx.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "wkqnob",
+  allowCypressEnv: false,
   viewportWidth: 1280,
   viewportHeight: 720,
   video: true,
